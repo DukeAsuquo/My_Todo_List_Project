5ce2a52dc1507c1d829e736fe80f8639201ee921
DukeAsuquo <dukeasuquo@gmail.com>
2023-10-19 20:17:10 +0100

    My todo list project
    My todo list project built with EJS and Express

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const ListItem = []; 
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+  
+});
+
+app.post("/submit", (req, res) => {
+    const Item = req.body["item"];
+    ListItem.push(Item);
+  res.render("index.ejs", {ListItem});
+  
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
